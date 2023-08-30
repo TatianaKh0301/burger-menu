0555b1544ae49ac88a3f0b7f182d23169f5d546c
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-08-30 15:26:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,23 +46,23 @@ if (isMobile.any()) {
 
 // Scroll on click
 
-// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-// if(menuLinks.length > 0) {
-//     menuLinks.forEach(menuLink => {
-//         menuLink.addEventListener("click", onMenuLinkClick);
-//     });
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
 
-//     function onMenuLinkClick(e) {
-//         const menuLink = e.target;
-//         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-//             const gotoBlock = document.querySelector(menuLink.dataset.goto);
-//             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
-//             window.scrollTo({
-//                 top: gotoBlockValue,
-//                 behavior: "smooth"
-//             });
-//             e.preventDefault();
-//         }
-//     }
-// } 
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+} 
