edea3d7c31a1e3beb8427dcab942245bca9177b9
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-08-31 10:31:46 +0300

    finish menu burger

--- js/script.js
+++ js/script.js
@@ -43,6 +43,17 @@ if (isMobile.any()) {
     document.body.classList.add('_pc');
 }
 
+// Menu burger
+
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {    
+    iconMenu.addEventListener("click", function(e){
+        document.body.classList.toggle("_lock");
+        iconMenu.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    });
+}
 
 // Scroll on click
 
@@ -57,6 +68,12 @@ if(menuLinks.length > 0) {
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains("_active")) {
+                document.body.classList.remove("_lock");
+                iconMenu.classList.remove("_active");
+                menuBody.classList.remove("_active");
+            }
 
             window.scrollTo({
                 top: gotoBlockValue,
